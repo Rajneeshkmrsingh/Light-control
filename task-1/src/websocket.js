8e187fb3b3081d001fca9a3023a1d9cbd665c97c
Kumar@mk19998 <kumar.mk19998@gmail.com>
2023-07-22 17:12:57 +0530

    first-commit

--- task-1/src/websocket.js
+++ task-1/src/websocket.js
@@ -1,17 +1,14 @@
-// src/websocket.js
 import io from 'socket.io-client';
 import { updateBedroomState } from './redux/bedroomSlice';
 
-const socket = io('ws://localhost:8080'); // Replace with your WebSocket server URL
+const socket = io('ws://localhost:8080'); 
 
 export const createWebSocketConnection = store => {
-  // Subscribe to changes from WebSocket
   socket.on('bedroom_state', data => {
     store.dispatch(updateBedroomState(data));
   });
 };
 
-// Emit WebSocket messages when buttons are clicked
 export const toggleLight1 = () => {
   socket.emit('toggle_light1');
 };
